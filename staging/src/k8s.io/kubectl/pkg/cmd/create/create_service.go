f3205f34f545f0e4113d95ce5ebe1d7deb80790a
ramnar <raam.soft@gmail.com>
2020-11-17 10:13:31 +0530

    updated code

--- staging/src/k8s.io/kubectl/pkg/cmd/create/create_service.go
+++ staging/src/k8s.io/kubectl/pkg/cmd/create/create_service.go
@@ -63,14 +63,59 @@ func addPortFlags(cmd *cobra.Command) {
 
 // ServiceClusterIPOpts holds the options for 'create service clusterip' sub command
 type ServiceClusterIPOpts struct {
-	CreateSubcommandOptions *CreateSubcommandOptions
+	// PrintFlags holds options necessary for obtaining a printer
+	PrintFlags *genericclioptions.PrintFlags
+	PrintObj   func(obj runtime.Object) error
+	// Name of resource being created
+	Name             string
+	DefaultName      string
+	Selector         string
+	// Port will be used if a user specifies --port OR the exposed object
+	// has one port
+	Port             string
+	// Ports will be used if a user doesn't specify --port AND the
+	// exposed object has multiple ports
+	Ports            string
+	Labels           string
+	ExternalIP       string
+	LoadBalancerIP   string
+	Type             string
+	Protocol         string
+	// protocols will be used to keep port-protocol mapping derived from
+	// exposed object
+	Protocols        string
+	// alias of TargetPort
+	ContainerPort    string
+	TargetPort       string
+	PortName         string
+	SessionAffinity  string
+	ClusterIP        string
+	
+	DryRunStrategy   cmdutil.DryRunStrategy
+	DryRunVerifier   *resourcecli.DryRunVerifier
+	CreateAnnotation bool
+	FieldManager     string
+
+	Namespace        string
+	EnforceNamespace bool
+
+	Mapper meta.RESTMapper
+	Client *coreclient.CoreV1Client
+
+	genericclioptions.IOStreams
+}
+
+// NewServiceClusterIPOpts creates a new *ServiceClusterIPOpts with sane defaults
+func NewServiceClusterIPOpts(ioStreams genericclioptions.IOStreams) *ServiceClusterIPOpts {
+	return &ServiceClusterIPOpts{
+		PrintFlags: genericclioptions.NewPrintFlags("created").WithTypeSetter(scheme.Scheme),
+		IOStreams:  ioStreams,
+	}
 }
 
 // NewCmdCreateServiceClusterIP is a command to create a ClusterIP service
 func NewCmdCreateServiceClusterIP(f cmdutil.Factory, ioStreams genericclioptions.IOStreams) *cobra.Command {
-	options := &ServiceClusterIPOpts{
-		CreateSubcommandOptions: NewCreateSubcommandOptions(ioStreams),
-	}
+	o := NewServiceClusterIPOpts(ioStreams)
 
 	cmd := &cobra.Command{
 		Use:                   "clusterip NAME [--tcp=<port>:<targetPort>] [--dry-run=server|client|none]",
@@ -79,19 +124,20 @@ func NewCmdCreateServiceClusterIP(f cmdutil.Factory, ioStreams genericclioptions
 		Long:                  serviceClusterIPLong,
 		Example:               serviceClusterIPExample,
 		Run: func(cmd *cobra.Command, args []string) {
-			cmdutil.CheckErr(options.Complete(f, cmd, args))
-			cmdutil.CheckErr(options.Run())
+			cmdutil.CheckErr(o.Complete(f, cmd, args))
+			cmdutil.CheckErr(o.Validate())
+			cmdutil.CheckErr(o.Run())
 		},
 	}
 
-	options.CreateSubcommandOptions.PrintFlags.AddFlags(cmd)
-
+    o.PrintFlags.AddFlags(cmd)
+    
 	cmdutil.AddApplyAnnotationFlags(cmd)
 	cmdutil.AddValidateFlags(cmd)
-	cmdutil.AddGeneratorFlags(cmd, generateversioned.ServiceClusterIPGeneratorV1Name)
+	cmdutil.AddDryRunFlag(cmd)
 	addPortFlags(cmd)
 	cmd.Flags().String("clusterip", "", i18n.T("Assign your own ClusterIP or set to 'None' for a 'headless' service (no loadbalancing)."))
-	cmdutil.AddFieldManagerFlagVar(cmd, &options.CreateSubcommandOptions.FieldManager, "kubectl-create")
+	cmdutil.AddFieldManagerFlagVar(cmd, &o.FieldManager, "kubectl-create")
 	return cmd
 }
 
@@ -106,25 +152,162 @@ func (o *ServiceClusterIPOpts) Complete(f cmdutil.Factory, cmd *cobra.Command, a
 		return err
 	}
 
-	var generator generate.StructuredGenerator
-	switch generatorName := cmdutil.GetFlagString(cmd, "generator"); generatorName {
-	case generateversioned.ServiceClusterIPGeneratorV1Name:
-		generator = &generateversioned.ServiceCommonGeneratorV1{
-			Name:      name,
-			TCP:       cmdutil.GetFlagStringSlice(cmd, "tcp"),
-			Type:      v1.ServiceTypeClusterIP,
-			ClusterIP: cmdutil.GetFlagString(cmd, "clusterip"),
-		}
-	default:
-		return errUnsupportedGenerator(cmd, generatorName)
+
+	restConfig, err := f.ToRESTConfig()
+	if err != nil {
+		return err
+	}
+	o.Client, err = coreclient.NewForConfig(restConfig)
+	if err != nil {
+		return err
 	}
 
-	return o.CreateSubcommandOptions.Complete(f, cmd, args, generator)
+	o.CreateAnnotation = cmdutil.GetFlagBool(cmd, cmdutil.ApplyAnnotationsFlag)
+
+	o.DryRunStrategy, err = cmdutil.GetDryRunStrategy(cmd)
+	if err != nil {
+		return err
+	}
+	dynamicClient, err := f.DynamicClient()
+	if err != nil {
+		return err
+	}
+	discoveryClient, err := f.ToDiscoveryClient()
+	if err != nil {
+		return err
+	}
+	o.DryRunVerifier = resourcecli.NewDryRunVerifier(dynamicClient, discoveryClient)
+
+	o.Namespace, o.EnforceNamespace, err = f.ToRawKubeConfigLoader().Namespace()
+	if err != nil {
+		return err
+	}
+
+	cmdutil.PrintFlagsWithDryRunStrategy(o.PrintFlags, o.DryRunStrategy)
+
+	printer, err := o.PrintFlags.ToPrinter()
+	if err != nil {
+		return err
+	}
+
+	o.PrintObj = func(obj runtime.Object) error {
+		return printer.PrintObj(obj, o.Out)
+	}
+
+	return nil
+}
+
+// Validate checks to the ServiceClusterIPOpts to see if there is sufficient information run the command.
+func (o *ServiceClusterIPOpts) Validate() error {
+	if len(o.Name) == 0 {
+		return fmt.Errorf("name must be specified")
+	}
+	
+	if len(o.Selector) == 0 {
+		return fmt.Errorf("'selector' is a required parameter")
+	}
+	return nil
 }
 
 // Run calls the CreateSubcommandOptions.Run in ServiceClusterIPOpts instance
 func (o *ServiceClusterIPOpts) Run() error {
-	return o.CreateSubcommandOptions.Run()
+	service, err := o.createService()
+	if err != nil {
+		return err
+	}
+
+	if err := util.CreateOrUpdateAnnotation(o.CreateAnnotation, service, scheme.DefaultJSONEncoder()); err != nil {
+		return err
+	}
+
+	if o.DryRunStrategy != cmdutil.DryRunClient {
+		createOptions := metav1.CreateOptions{}
+		if o.FieldManager != "" {
+			createOptions.FieldManager = o.FieldManager
+		}
+		if o.DryRunStrategy == cmdutil.DryRunServer {
+			if err := o.DryRunVerifier.HasSupport(service.GroupVersionKind()); err != nil {
+				return err
+			}
+			createOptions.DryRun = []string{metav1.DryRunAll}
+		}
+		service, err = o.Client.Service(o.Namespace).Create(context.TODO(), service, createOptions)
+		if err != nil {
+			return fmt.Errorf("failed to create service: %v", err)
+		}
+	}
+	return o.PrintObj(service)
+}
+
+func (o *ServiceClusterIPOpts) createService() (*corev1.Service, error) {
+	namespace := ""
+	if o.EnforceNamespace {
+		namespace = o.Namespace
+	}
+	service := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{APIVersion: corev1.SchemeGroupVersion.String(), Kind: "Service"},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   o.name,
+			Namespace: namespace,
+			Labels: o.labels,
+		},
+		Spec: &corev1.ServiceSpec{
+			Selector: o.selector,
+			Ports:    o.ports,
+		},
+	}
+
+	targetPortString := o.TargetPort
+	if len(targetPortString) == 0 {
+		targetPortString = o.ContainerPort
+	}
+	if len(targetPortString) > 0 {
+		var targetPort intstr.IntOrString
+		if portNum, err := strconv.Atoi(targetPortString); err != nil {
+			targetPort = intstr.FromString(targetPortString)
+		} else {
+			targetPort = intstr.FromInt(portNum)
+		}
+		// Use the same target-port for every port
+		for i := range service.Spec.Ports {
+			service.Spec.Ports[i].TargetPort = targetPort
+		}
+	} else {
+		// If --target-port or --container-port haven't been specified, this
+		// should be the same as Port
+		for i := range service.Spec.Ports {
+			port := service.Spec.Ports[i].Port
+			service.Spec.Ports[i].TargetPort = intstr.FromInt(int(port))
+		}
+	}
+	if len(o.ExternalIP) > 0 {
+		service.Spec.ExternalIPs = []string{o.ExternalIP}
+	}
+	if len(o.Type) != 0 {
+		service.Spec.Type = &corev1.ServiceType(o.Type)
+	}
+	if service.Spec.Type == &corev1.ServiceTypeLoadBalancer {
+		service.Spec.LoadBalancerIP = o.LoadBalancerIP
+	}
+	if len(o.SessionAffinity) != 0 {
+		switch &corev1.ServiceAffinity(o.SessionAffinity) {
+		case &corev1.ServiceAffinityNone:
+			service.Spec.SessionAffinity = &corev1.ServiceAffinityNone
+		case &corev1.ServiceAffinityClientIP:
+			service.Spec.SessionAffinity = &corev1.ServiceAffinityClientIP
+		default:
+			return nil, fmt.Errorf("unknown session affinity: %s", o.SessionAffinity)
+		}
+	}
+	if len(o.ClusterIP) != 0 {
+		if o.ClusterIP == "None" {
+			service.Spec.ClusterIP = &corev1.ClusterIPNone
+		} else {
+			service.Spec.ClusterIP = o.ClusterIP
+		}
+	}
+
+	return service, nil
 }
 
 var (
